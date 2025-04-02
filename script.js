a4379359fefbe1816b9d2e2159723ab45404978d
Pedro Pinheiro <pedroph12@gmail.com>
2025-04-01 21:52:34 -0300

    primeiro push

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('.pointer').forEach((pointer)=>{
+    pointer.addEventListener('click', changeSlide)
+});
+
+function changeSlide(event){
+    let pointer = event.target.getAttribute('data-item')
+    let actualSlider = document.querySelector('.banner .sliders')
+
+    if(pointer == 1){
+        actualSlider.style.marginLeft = '0'
+    }else if(pointer == 2){
+        actualSlider.style.marginLeft = '-100dvw'
+    }else if(pointer == 3){
+        actualSlider.style.marginLeft = '-200dvw'
+    }
+        
+
+}
+document.querySelector('.button.one').addEventListener('click', clicou)
+function clicou(){
+    
+    let tempo = 2
+    while(tempo > 0){
+        tempo = tempo -1
+        alert('voce ativou uma bomba ' + tempo) 
+
+        
+    };
+}
